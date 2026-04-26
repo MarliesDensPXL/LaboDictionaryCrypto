0822a906e2e906abc632b6f00b85225ff1d6715b
Marlies Dens <marlies.dens@pxl.be>
2026-04-26 20:32:22 +0200

    3.1 en 3.2

--- LaboDictionaryCrypto/Program.cs
+++ LaboDictionaryCrypto/Program.cs
@@ -4,8 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Dictionary<string, double> crypto = new Dictionary<string, double>(); 
+            //3.1
+            Dictionary<string, double> crypto = new Dictionary<string, double>
+            {
+                {"BTC", 0.5 },
+                {"ETH", 2 },
+                {"SOL", 10 }
+            };
 
+            //3.2
+            crypto.Add("XRP", 0.7);
+            crypto.Add("ADA", 5);
+
+            //3.1
+            foreach (var coin in crypto)
+            {
+                Console.WriteLine($"{coin.Key}: {coin.Value,4} coins");
+            }
 
         }
     }
