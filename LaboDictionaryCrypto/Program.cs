7223ff0778e305de0d34fb9c5491a2d1ae83fe2b
Marlies Dens <marlies.dens@pxl.be>
2026-04-26 20:15:48 +0200

    Add project files.

---
+++ LaboDictionaryCrypto/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LaboDictionaryCrypto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> crypto = new Dictionary<string, double>(); 
+
+
+        }
+    }
+}
